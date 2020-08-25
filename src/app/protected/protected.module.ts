822b583e51f0dfa76c6f057cb3353ee342363736
Marko1212 <shihonage7@gmail.com>
2020-08-25 17:44:34 +0200

    update

--- src/app/protected/protected.module.ts
+++ src/app/protected/protected.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
 
 import { ProtectedRoutingModule } from './protected-routing.module';
-
+import { DashboardModule } from './dashboard/dashboard.module';
+import { ParametersModule } from './parameters/parameters.module';
+import { PlanningModule } from './planning/planning.module';
+import { ProfilModule } from './profil/profil.module';
+import { WorkdayModule } from './workday/workday.module';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    ProtectedRoutingModule
-  ]
+  imports: [SharedModule, ProtectedRoutingModule, DashboardModule, ParametersModule, PlanningModule, ProfilModule, WorkdayModule],
 })
-export class ProtectedModule { }
+export class ProtectedModule {}
