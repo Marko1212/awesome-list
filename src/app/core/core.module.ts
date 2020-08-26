115d5ee93a77cea86af37aca9228eca019a1c658
Marko1212 <shihonage7@gmail.com>
2020-08-26 12:29:07 +0200

    update

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,16 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PublicModule } from '../public/public.module';
 import { ProtectedModule } from '../protected/protected.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, PublicModule, ProtectedModule],
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    PublicModule,
+    ProtectedModule,
+  ],
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
