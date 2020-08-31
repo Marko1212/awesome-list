cd78bad2119b2bc5400c8b5f4dc66cf0bcf9d7c2
Marko1212 <shihonage7@gmail.com>
2020-08-31 20:43:04 +0200

    update

--- src/app/protected/planning/planning-workday-item/planning-workday-item.component.ts
+++ src/app/protected/planning/planning-workday-item/planning-workday-item.component.ts
@@ -1,23 +1,46 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChange } from '@angular/core';
 
 @Component({
   selector: 'al-planning-workday-item',
   templateUrl: './planning-workday-item.component.html',
   styles: [],
 })
-export class PlanningWorkdayItemComponent {
-  private currentWorkday;
+export class PlanningWorkdayItemComponent implements OnChanges {
+  @Input() dueDate: string;
+  @Input() doneTasks: number | string;
+  @Input() remainingTasks: number | string;
 
-  @Input()
-  set workday(workday) {
-    this.currentWorkday = workday || {};
-
-    if ('Lundi' === workday.dueDate) {
-      this.currentWorkday.dueDate += " (Aujourd'hui)";
+  ngOnChanges(changes: { [propKey: string]: SimpleChange }) {
+    for (const propName in changes) {
+      this.update(propName, changes[propName].currentValue);
     }
   }
 
-  get workday() {
-    return this.currentWorkday;
+  update(propName, propValue) {
+    switch (propName) {
+      case 'dueDate': {
+        if ('Lundi' === propValue) {
+          this.dueDate += " (Aujourd'hui)";
+        }
+        break;
+      }
+      case 'doneTasks': {
+        if (0 === propValue) {
+          this.doneTasks = 'Aucune tâche terminée.';
+        }
+        break;
+      }
+      case 'remainingTasks': {
+        if (0 === propValue) {
+          this.remainingTasks = 'Journée de travail terminée !';
+        }
+        break;
+      }
+      default: {
+        break;
+      }
+    }
   }
+
+  removeWorkday(date) {}
 }
--- src/app/protected/planning/planning-workday-list/planning-workday-list.component.ts
+++ src/app/protected/planning/planning-workday-list/planning-workday-list.component.ts
@@ -12,9 +12,10 @@ export class PlanningWorkdayListComponent implements OnInit {
   public workdays$;
   constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.workdays$ = of([
-      { dueDate: 'Lundi', doneTasks: 1, remainingTasks: 3 },
+      // Je passe remainingTasks à 0 pour tester mon composant fils :
+      { dueDate: 'Lundi', doneTasks: 1, remainingTasks: 0 },
       { dueDate: 'Mardi', doneTasks: 0, remainingTasks: 2 },
       { dueDate: 'Mercredi', doneTasks: 0, remainingTasks: 1 },
     ]).pipe(delay(1000));
