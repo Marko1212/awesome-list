f74a203e4b83d4aa0fcb614058637e8bcc84ae25
Marko1212 <shihonage7@gmail.com>
2020-09-05 12:44:45 +0200

    change

--- src/app/public/home/home.module.ts
+++ src/app/public/home/home.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { HomeBannerComponent } from './home-banner/home-banner.component';
 import { HomeFeaturesComponent } from './home-features/home-features.component';
 import { HomeFeatureCardComponent } from './home-feature-card/home-feature-card.component';
+//import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxBootstrapModule } from './modules/ngx-bootstrap.module';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [SidenavComponent],
   imports: [CommonModule, NgxBootstrapModule],
-  exports: [CommonModule, NgxBootstrapModule],
+  exports: [CommonModule, NgxBootstrapModule, SidenavComponent],
 })
 export class SharedModule {}
