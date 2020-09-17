4c62fbbf7b31653fa560a40cbc1dd928efa573e5
Marko1212 <shihonage7@gmail.com>
2020-09-17 15:09:44 +0200

    update

--- src/app/protected/workday/workday-form-tasks/workday-form-tasks.component.ts
+++ src/app/protected/workday/workday-form-tasks/workday-form-tasks.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'al-workday-form-tasks',
@@ -8,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkdayFormTasksComponent implements OnInit {
 
-  constructor() { }
+  @Input() tasks: FormArray;
+  @Input() workdayForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
+
+  onAddedTask() {
+    const taskGroup = this.fb.group({
+     'title': ''
+    });
+    this.tasks.push(taskGroup);
+   }
+
+   onRemovedTask(index: number) {
+    this.tasks.removeAt(index);
+   }
 
 }
