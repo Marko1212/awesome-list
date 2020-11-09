901a056e7d358a23585b3a54fdf2bc8ba463126f
Marko1212 <shihonage7@gmail.com>
2020-11-09 12:15:22 +0100

    update

--- src/app/core/services/workdays.service.ts
+++ src/app/core/services/workdays.service.ts
@@ -179,4 +179,25 @@ export class WorkdaysService {
       },
     };
   }
+
+  update(workday: Workday) {
+    const url = `${environment.firebase.firestore.baseURL}/workdays/${workday.id}?key=${environment.firebase.apiKey}&currentDocument.exists=true`;
+    const data = this.getWorkdayForFirestore(workday);
+    const jwt: string = localStorage.getItem('token');
+    const httpOptions = {
+     headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      'Authorization': `Bearer ${jwt}`
+     })
+    };
+    
+    return this.http.patch(url, data, httpOptions).pipe(
+     tap(_ => this.toastrService.showToastr({
+      category: 'success',
+      message: 'Votre journée de travail a été sauvegardé avec succès.'
+     })),
+     catchError(error => this.errorService.handleError(error)),
+     finalize(() => this.loaderService.setLoading(false))
+    );
+   }
 }
